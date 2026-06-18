67defb95df80bf33686c430c4f56a77a08ea65b5
Mibrahim0941 <mi094131@gmail.com>
2026-06-18 12:08:31 +0500

    Initial Push

---
+++ src/main/java/org/example/model/Student.java
@@ -0,0 +1,10 @@
+package org.example.model;
+import java.util.Date;
+
+class Student{
+    private String StudentID;
+    protected String Name;
+    protected String Email;
+    protected Date DoB;
+    protected String PhoneNum;
+}
